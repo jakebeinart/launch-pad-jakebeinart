a578d51cc4d459cdf5f643a567392a01020ad1fd
Jake Beinart <jsb8bd@mst.edu>
2014-09-28 12:33:57 -0500

    Added PropertyChangedBase class to ConsoleViewModel

--- LaunchPadjakebeinart/LaunchPadjakebeinart/ViewModels/ConsoleViewModel.cs
+++ LaunchPadjakebeinart/LaunchPadjakebeinart/ViewModels/ConsoleViewModel.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using LaunchPadjakebeinart.Models;
+using Caliburn.Micro.PropertyChangedBase;
 
 namespace LaunchPadjakebeinart.ViewModels
 {
