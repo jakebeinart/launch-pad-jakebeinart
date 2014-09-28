a51e041288c10348b3069dd7fcfd768ae087f8c2
Jake Beinart <jsb8bd@mst.edu>
2014-09-28 12:29:43 -0500

    Create Models in ConsoleViewModel and MainWindowModel

--- LaunchPadjakebeinart/LaunchPadjakebeinart/ViewModels/ConsoleViewModel.cs
+++ LaunchPadjakebeinart/LaunchPadjakebeinart/ViewModels/ConsoleViewModel.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LaunchPadjakebeinart.Models;
 
 namespace LaunchPadjakebeinart.ViewModels
 {
     class ConsoleViewModel
     {
-        
+        private ConsoleModel Model;
     }
 }
